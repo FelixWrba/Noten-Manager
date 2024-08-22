5ecc5934f57d8777ca06e2fffa970b878d6c5afb
Felix Wrba <felix.wrba@web.de>
2024-08-22 19:41:33 +0200

    Added Object oriented code and comments [1/2]

--- js/welcome.js
+++ js/welcome.js
@@ -1,35 +1,70 @@
+// defines the class Subject()
+class Subject {
+    id;
+    name;
+    sel = false;
+    constructor(name, id = name) {
+        this.name = name;
+        this.id = id;
+    }
+}
+
+// makes sure that the top of the page is shown
+document.getElementById('top').scrollIntoView({ 'behavior': 'smooth' });
+
+// get #main
 let main = document.querySelector('main');
-let subjects = [{ "id": "german", "name": "Deutsch", "sel": false }, { "id": "english", "name": "Englisch", "sel": false }, { "id": "french", "name": "Franz&ouml;sisch", "sel": false }, { "id": "latin", "name": "Latein", "sel": false }, { "id": "greece", "name": "Altgriechisch", "sel": false }, { "id": "spain", "name": "Spanisch", "sel": false }, { "id": "maths", "name": "Mathe", "sel": false }, { "id": "informathic", "name": "Informatik", "sel": false }, { "id": "natur", "name": "Naturwissenschaften", "sel": false }, { "id": "tech", "name": "Technik", "sel": false }, { "id": "music", "name": "Musik", "sel": false }, { "id": "art", "name": "Kunst", "sel": false }, { "id": "religion", "name": "Etik / Religion", "sel": false }, { "id": "pe", "name": "Sport", "sel": false }];
+// creates all the subjects that are needed for the manager
+let subjects = [
+    new Subject("Mathe", "maths"),
+    new Subject("Englisch", "english"),
+    new Subject("Französisch", "french"),
+    new Subject("Latein", "latin"),
+    new Subject("Altgriechisch", "greece"),
+    new Subject("Spanisch", "spain"),
+    new Subject("Mathe", "maths"),
+    new Subject("Informatik", "informathic"),
+    new Subject("Naturwissenschaften", "natur"),
+    new Subject("Technik", "tech"),
+    new Subject("Musik", "music"),
+    new Subject("Kunst", "art"),
+    new Subject("Etik / Religion", "religion"),
+    new Subject("Sport", "pe")
+];
 
-document.getElementById('top').scrollIntoView({ 'behavior': 'smooth'});
-
+// shows the subject selection after the "Jetzt loslegen" button is clicked
 function showMain() {
     main.style.opacity = '1';
     main.scrollIntoView({ 'behavior': 'smooth' });
 }
 
+// adds a custom subject to the HTML list and to the subjects array
 function addSubject(subjectAddName = undefined) {
-    if (!subjectAddName && subjectAddName == '') {
+    // check if subjectAddName is undefined/empty => if undefined empty cancel adding
+    if (!subjectAddName || subjectAddName == '') {
         alert('Du must ein Fach eingeben!');
         return;
     }
-    subjects.forEach(e => {
-        if (e.id == subjectAddName) {
-            alert(`Das Fach ${subjectAddName} wurde bereites hinzugefügt!`);
-            return;
-        }
-    });
+    // cancel adding if condition true
+    if (
+        // loop through subjects to check if subjectAddName in list
+        subjects.forEach(e => {
+            // if in list return true => cancels adding
+            if (e.id == subjectAddName) {
+                alert(`Das Fach ${subjectAddName} wurde bereites hinzugefügt!`);
+                return true;
+            }
+            // else continue with adding process
+            else return false;
+        })
+    ) return;
     document.getElementById('addedSubjects').innerHTML += `
     <span>
         <label for="${subjectAddName}">${subjectAddName}: </label>
         <input type="checkbox" id="${subjectAddName}" checked>
     </span>
     `;
-    subjects.push({
-        "id": subjectAddName,
-        "name": subjectAddName,
-        "sel": true
-    });
+    subjects.push(new Subject(subjectAddName));
 }
 
 function createTimeTable() {
